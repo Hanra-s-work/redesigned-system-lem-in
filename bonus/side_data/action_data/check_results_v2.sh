86dd68f394ad94ed8ef23790b4e8fd822788bbba
Henry LETELLIER <henry.letellier@epitech.eu>
2023-03-20 20:02:00 +0100

    [FIXED] the problem should have been fixed

--- bonus/side_data/action_data/check_results_v2.sh
+++ bonus/side_data/action_data/check_results_v2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # (c) Written by Henry Letellier
 CONTENT="$(cat coding-style-reports.log)"
+if [ ${#CONTENT} -eq 0 ]; then
+    echo "::success file='',line=1,endLine=0,title=No coding style error detected"
+    exit 0
+fi
 ILLEGAL_FILES_FOUND=0
 while IFS= read -r LINE; do
     echo "::error file=$LINE,line=1,endLine=0,title=Unwanted file detected::$LINE"
