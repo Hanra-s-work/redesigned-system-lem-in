d1dbcd2559e8fe4f67293ce5ae4642290b8a71fd
Eric <eric1.xu@epitech.eu--global user.email eric1.xu@epitech.eugit config --global user.email eric1.xu@epitech.eu>
2023-04-06 21:10:01 +0200

    [ADD] disp_parser.c, disp_rooms.c, disp_tunnels.c

---
+++ src/disp/disp_rooms.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2023
+** disp_room
+** File description:
+** cpe
+*/
+
+#include "my.h"
+#include "li_structs.h"
+#include "linked_lists.h"
+#include "batch_colouriser.h"
+#include "li_structs_parser.h"
+#include "li_colour_constants.h"
+
+void disp_room(bcolours_t *bc, llists_t *room)
+{
+    llists_t *tmp = NULL;
+    rooms_t *tmp_room = NULL;
+
+    for (tmp = room; tmp != NULL; tmp = tmp->next) {
+        tmp_room = (rooms_t *)tmp->data;
+        set_colour(bc, INFO_COL, "name = ");
+        my_putstr(tmp_room->name);
+        set_colour(bc, INFO_COL, "\nposx = ");
+        my_put_nbr(tmp_room->posx);
+        set_colour(bc, INFO_COL, "\nposy = ");
+        my_put_nbr(tmp_room->posy);
+        set_colour(bc, INFO_COL, "\nis_start = ");
+        my_put_nbr(tmp_room->is_start);
+        set_colour(bc, INFO_COL, "\nis_end = ");
+        my_put_nbr(tmp_room->is_end);
+        set_colour(bc, INFO_COL, "\nnb_occupents = ");
+        my_put_nbr(tmp_room->nb_occupents);
+        set_colour(bc, INFO_COL, "\nnb_max_occupents = ");
+        my_put_nbr(tmp_room->nb_max_occupents);
+        set_colour(bc, RESET_COL, "\n");
+    }
+}
---
+++ src/disp/disp_tunnels.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2023
+** disp_tunnels
+** File description:
+** cpe
+*/
+
+#include "my.h"
+#include "li_structs.h"
+#include "linked_lists.h"
+#include "batch_colouriser.h"
+#include "li_structs_parser.h"
+#include "li_colour_constants.h"
+
+void disp_tunnels(bcolours_t *bc, llists_t *tunnel)
+{
+    llists_t *tmp = NULL;
+    tunnel_t *tmp_tunnel = NULL;
+    
+    for (tmp = tunnel; tmp != NULL; tmp = tmp->next) {
+        tmp_tunnel = (tunnel_t *)tmp->data;
+        set_colour(bc, INFO_COL, "start  = ");
+        my_put_nbr(tmp_tunnel->start);
+        set_colour(bc, RESET_COL, "\n");
+        set_colour(bc, INFO_COL, "end    = ");
+        my_put_nbr(tmp_tunnel->end);
+        set_colour(bc, RESET_COL, "\n");
+        set_colour(bc, INFO_COL, "weight = ");
+        my_put_nbr(tmp_tunnel->weight);
+        set_colour(bc, RESET_COL, "\n");
+    }
+}
