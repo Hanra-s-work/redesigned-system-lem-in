1506233e41e3ab390a168c2f871ded0107659b5f
Henry LETELLIER <henry.letellier@epitech.eu>
2023-04-12 13:29:56 +0200

    [REMOVED] the colour for the main branch

--- src/sub_main.c
+++ src/sub_main.c
@@ -35,7 +35,7 @@ int sub_main(int argc, char **argv)
 
     parsed = parse_main();
     status = path_finding_main(parsed);
-    disp_pretty_output(bc, parsed->file_content);
+    disp_file_content(parsed->file_content);
     free_colours(bc);
     free_parser(parsed);
     return status;
